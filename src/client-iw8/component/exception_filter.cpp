651fd8f278ffaffa0d943ef879e0728c2463b160
xifil <iamspaceyt@gmail.com>
2025-04-24 17:47:56 +0100

    why does the exception filter crash game? idk mane

--- src/client-iw8/component/exception_filter.cpp
+++ src/client-iw8/component/exception_filter.cpp
@@ -14,9 +14,18 @@ namespace exception_filter {
 	struct component final : generic_component {
 		void post_load() override {
 			utils::nt::library game{};
-			utils::hook::set(game.get_iat_entry("kernel32.dll", "SetUnhandledExceptionFilter"), set_unhandled_exception_filter_stub);
+//			utils::hook::set(game.get_iat_entry("kernel32.dll", "SetUnhandledExceptionFilter"), set_unhandled_exception_filter_stub);
+
+			auto entry = game.get_iat_entry("kernel32.dll", "SetUnhandledExceptionFilter");
+
+			DWORD protect;
+			VirtualProtect(reinterpret_cast<void*>(entry), sizeof(*entry), PAGE_EXECUTE_READWRITE, &protect);
+			//this->m_Original = *entry;
+			//T::m_Original = reinterpret_cast<decltype(T::m_Original)>(this->m_Original);
+			*entry = set_unhandled_exception_filter_stub;
+			VirtualProtect(reinterpret_cast<void*>(entry), sizeof(*entry), protect, &protect);
 		}
 	};
 }
 
-REGISTER_COMPONENT(exception_filter::component)
+//REGISTER_COMPONENT(exception_filter::component)
