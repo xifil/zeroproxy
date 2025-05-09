5f8c8e5e8f48fdb69ad6b4928cb9791fbe4a213a
xifil <iamspaceyt@gmail.com>
2025-05-09 21:34:07 +0100

    bababa

--- src/client-iw4-store/component/patches.cpp
+++ src/client-iw4-store/component/patches.cpp
@@ -6,14 +6,34 @@
 
 #include <loader/component_loader.hpp>
 #include <utils/hook.hpp>
+#include <utils/string.hpp>
 
 #include <version.hpp>
 
 namespace patches {
 	namespace {
+		utils::hook::detour initialize_api_impl_hook;
+		utils::hook::detour initialize_api_impl_ex_hook;
+		utils::hook::detour query_api_impl_hook;
 		utils::hook::detour x_store_query_game_license_result_hook;
 
 		utils::hook::iat_detour create_window_ex_a_hook;
+		utils::hook::iat_detour get_proc_address_hook;
+
+		HRESULT initialize_api_impl_stub(std::uint64_t a1, std::uint64_t a2) {
+			LOG("Component/Patches", DEBUG, "[" __FUNCTION__ "](0x{:016X}, 0x{:016X})", a1, a2);
+			return initialize_api_impl_hook.invoke<HRESULT>(a1, a2);
+		}
+
+		HRESULT initialize_api_impl_ex_stub(std::uint64_t a1, std::uint64_t a2, std::uint64_t a3) {
+			LOG("Component/Patches", DEBUG, "[" __FUNCTION__ "](0x{:016X}, 0x{:016X}, 0x{:016X})", a1, a2, a3);
+			return initialize_api_impl_ex_hook.invoke<HRESULT>(a1, a2, a3);
+		}
+
+		HRESULT query_api_impl_stub(std::uint64_t a1, std::uint64_t a2, std::uint64_t a3) {
+			LOG("Component/Patches", DEBUG, "[" __FUNCTION__ "]({}, {}, {})", and_rel(a1), and_rel(a2), and_rel(a3));
+			return query_api_impl_hook.invoke<HRESULT>(a1, a2, a3);
+		}
 
 		HRESULT x_store_query_game_license_result_stub(void* async, uwp::XStoreGameLicense* license) {
 			// result ignored, we return S_OK anyway to avoid "popup_drm_menu_gdk_license_error"
@@ -38,6 +58,24 @@ namespace patches {
 			return create_window_ex_a_hook.invoke<HWND>(dw_ex_style, lp_class_name, lp_window_name_patched, dw_style, x, y, n_width, n_height, hwnd_parent,
 				h_menu, h_instance, lp_param);
 		}
+
+		FARPROC get_proc_address_stub(HMODULE h_module, LPCSTR lp_proc_name) {
+			auto res = get_proc_address_hook.invoke<FARPROC>(h_module, lp_proc_name);
+			utils::nt::library lib(h_module);
+			if (lib.is_valid() && utils::string::to_lower(lib.get_name()) == "xgameruntime.dll") {
+				LOG("Component/Patches", DEBUG, "GetProcAddr: {}/{} = {}", lib.get_name(), lp_proc_name, and_rel(PTR_AS(std::uintptr_t, res)));
+				if ("InitializeApiImpl"s == lp_proc_name) {
+					initialize_api_impl_hook.create(res, initialize_api_impl_stub);
+				}
+				else if ("InitializeApiImplEx"s == lp_proc_name) {
+					initialize_api_impl_ex_hook.create(res, initialize_api_impl_ex_stub);
+				}
+				else if ("QueryApiImpl"s == lp_proc_name) {
+					query_api_impl_hook.create(res, query_api_impl_stub);
+				}
+			}
+			return res;
+		}
 	}
 
 	struct component final : generic_component {
@@ -45,6 +83,7 @@ namespace patches {
 			x_store_query_game_license_result_hook.create(game::XStoreQueryGameLicenseResult, x_store_query_game_license_result_stub);
 			
 			create_window_ex_a_hook.create(utils::nt::library(), "user32.dll", "CreateWindowExA", create_window_ex_a_stub);
+			get_proc_address_hook.create(utils::nt::library(), "kernel32.dll", "GetProcAddress", get_proc_address_stub);
 
 			/*
 			localized_strings::override(std::regex(".*"), [](const localized_strings::original_localization& original, const std::smatch&) {
